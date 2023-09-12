7f3caca47136d8f92441052eeea9834f1020b2d5
platisam <platisam@gmail.com>
2023-09-12 10:53:51 +0200

    setting states and section for title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React, { useState } from "react";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+
 function App() {
-  return <div className="App">Slider App</div>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+    </section>
+  );
 }
 
 export default App;
