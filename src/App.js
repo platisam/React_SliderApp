8ec422c25d5fe7d467af40a436236f41dc5808f7
platisam <platisam@gmail.com>
2023-09-12 11:30:03 +0200

    mapping through people array and displaying image, title and quote

--- src/App.js
+++ src/App.js
@@ -14,6 +14,20 @@ function App() {
           <span>/</span>reviews
         </h2>
       </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 }
