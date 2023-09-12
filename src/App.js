129e41197e0b336dc5f90a54541b6b0a8e398b2f
platisam <platisam@gmail.com>
2023-09-12 15:06:44 +0200

    setting useEffect hook for negative index condition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   FaChevronRight,
   FaChevronLeft,
@@ -11,6 +11,16 @@ import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
 
   return (
     <section className="section">
